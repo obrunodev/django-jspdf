1329b72a7b3f48a7fce9d574b03d172950ecf358
obrunodev <brunorpdev@gmail.com>
2022-10-18 17:24:48 -0300

    fix: Not receiving file content

--- sign/views/docusign.py
+++ sign/views/docusign.py
@@ -30,25 +30,23 @@ def docusign_signature(request):
             base_url = 'https://account-d.docusign.com/oauth/token'
             r = requests.post(base_url, data=post_data)
             token = r.json()
-            signer_email = request.POST.get('signer_email')
-            signer_name = request.POST.get('signer_name')
-            cc_email = request.POST.get('cc_email')
-            cc_name = request.POST.get('cc_name')
-            # document_file = request.POST.get('document_file')
-            # print(document_file)
-            # signer_type = request.POST.get('type', '')
-            with open(os.path.join(BASE_DIR, 'docusign_files/', 'test_file.pdf'), 'rb') as file:
-                content_bytes = file.read()
-            base64_file_content = base64.b64encode(content_bytes).decode('ascii')
-            # if signer_type != 'email':
-            #     return JsonResponse({"msg": "Tipo de assinatura inexistente."})
-            envelope_id = signature_by_email(token, base64_file_content, signer_email, signer_name, cc_name, cc_email)
+            contractor_name = f"{request.POST.get('contractor_name')} {request.POST.get('contractor_surname')}"
+            contractor_email = request.POST.get('contractor_email')
+            hired_name = request.POST.get('hired_name')
+            hired_email = request.POST.get('hired_email')
+            witness_name = f"{request.POST.get('witness_name')} {request.POST.get('witness_surname')}"
+            witness_email = request.POST.get('witness_email')
+            document_file = request.FILES['document_file'].read()
+            base64_file_content = base64.b64encode(document_file).decode('ascii')
+            envelope_id = signature_by_email(token, base64_file_content,
+                                             contractor_name, contractor_email,
+                                             hired_name, hired_email,
+                                             witness_name, witness_email)
             url = ''
             return JsonResponse({'docusign_url': url,
                                  'envelope_id': envelope_id,
                                  'message': 'O envelope foi criado na Docusign',
                                  'error': ''})
-            # return JsonResponse({'msg': 'Envelope criado!'})
         except Exception as e:
             return JsonResponse({'docsign_url': '',
                                 'envelope_id': '',
@@ -57,63 +55,83 @@ def docusign_signature(request):
 
 
 def make_envelope(base64_file_content, args):
-    """Cria a definição de envelope com seu conteúdo.
-    
-    - Utiliza um documento PDF para exemplificar.
+    """Cria a definição de envelope com seu conteúdo. Sendo eles, assinantes,
+    testemunhas, documentos e tabs.
     """
-    # Cria uma definição de envelope
-    env = EnvelopeDefinition(email_subject='Por favor, assine estes documentos.')
-    
-    # Cria o objeto documento
-    document = Document(document_base64=base64_file_content,
-                        name='Lorem ipsum',
-                        file_extension='pdf',
-                        document_id='1')
-    
-    # Insere os documentos no envelope
-    env.documents = [document]
-    
-    # Cria o model recipient do assinante
-    # TODO: Receber recipientes para endpoint
-    # Signer 1: Contratante
-    # Signer 2: Contratado
-    # Signer 3: Testemunha
-    signer = Signer(email=args['signer_email'],
-                    name=args['signer_name'],
-                    recipient_id='1',
-                    routing_order='1')
-    
-    # Cria um objeto cc que receberá a cópia dos documentos
-    cc = CarbonCopy(email=args['cc_email'],
-                    name=args['cc_name'],
-                    recipient_id='2',
-                    routing_order='2')
-    
-    # Cria o campo "Assine aqui" (tabs)
-    sign_here = SignHere(anchor_string='/sn/',
-                         anchor_units='pixels',
-                         anchor_y_offset='10',
-                         anchor_x_offset='20')
-    
-    # Adiciona as tabs (sign here) ao assinante 
-    signer.tabs = Tabs(sign_here_tabs=[sign_here])
-    
-    # Adiciona os recipientes no envelope
-    recipients = Recipients(signers=[signer], carbon_copies=[cc])
-    env.recipients = recipients
-    
-    # Mudar status do envelope
-    env.status = args['status']
-    
-    return env
-
-
-def signature_by_email(token, base64_file_content, signer_email, signer_name, cc_name, cc_email):
     try:
-        envelope_args = {'signer_email': signer_email,
-                         'signer_name': signer_name,
-                         'cc_email': cc_email,
-                         'cc_name': cc_name,
+        # Cria uma definição de envelope
+        env = EnvelopeDefinition(email_subject='Por favor, assine estes documentos.')
+        
+        # Cria o objeto documento
+        document = Document(document_base64=base64_file_content,
+                            name='Lorem ipsum',
+                            file_extension='pdf',
+                            document_id='1')
+        env.documents = [document]
+        
+        # === ↓ CREATING SIGNERS ↓ ===
+        contractor = Signer(email=args['contractor_email'],
+                            name=args['contractor_name'],
+                            recipient_id='1',
+                            routing_order='1')
+        hired = Signer(email=args['hired_email'],
+                       name=args['hired_name'],
+                       recipient_id='2',
+                       routing_order='2')
+        witness = Signer(email=args['witness_email'],
+                         name=args['witness_name'],
+                         recipient_id='3',
+                         routing_order='3')
+        # === ↑ CREATING SIGNERS ↑ ===
+        
+        # Cria o campo "Assine aqui" (tabs)
+        sign_here = SignHere(anchor_string='/sn/',
+                             anchor_units='pixels',
+                             anchor_y_offset='10',
+                             anchor_x_offset='20')
+        
+        # Adiciona as tabs (sign here) ao assinante 
+        contractor.tabs = Tabs(sign_here_tabs=[sign_here])
+        hired.tabs = Tabs(sign_here_tabs=[sign_here])
+        witness.tabs = Tabs(sign_here_tabs=[sign_here])
+
+        # Adiciona os recipientes no envelope
+        recipients = Recipients(signers=[contractor, hired, witness])
+        env.recipients = recipients
+        
+        # Mudar status do envelope
+        env.status = args['status']
+
+        return env
+    except Exception as e:
+        print(f'Erro: {e}')
+
+
+def signature_by_email(token, base64_file_content,
+                       contractor_name, contractor_email,
+                       hired_name, hired_email,
+                       witness_name, witness_email):
+    """Executa requisição para a API da docusign passando parâmetros de assinantes.
+    Cria envelope na plataforma e retorna o ID do envelope.
+    
+    Args:
+    * token: Objeto retornado da API docusign.
+    * base64_file_content: Arquivo do documento convertido.
+    * contractor_email: E-mail do contratante recebido via POST.
+    * contractor_name: Nome do contratante recebido via POST.
+    * hired_email: E-mail do contratado recebido via POST.
+    * hired_name: Nome do contratado recebido via POST.
+    * witness_email: E-mail da testemunha recebido via POST.
+    * witness_name: Nome da testemunha recebido via POST.
+    """
+    try:
+        # TODO(Bruno) Receber dados do contratado.
+        envelope_args = {'contractor_email': contractor_email,
+                         'contractor_name': contractor_name,
+                         'hired_email': hired_email,
+                         'hired_name': hired_name,
+                         'witness_email': witness_email,
+                         'witness_name': witness_name,
                          'status': 'sent'}
         envelope_definition = make_envelope(base64_file_content, envelope_args)
         try:
