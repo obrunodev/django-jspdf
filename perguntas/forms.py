672d87caa18d8cb2c3167d4ae781266d85e07f1f
obrunodev <brunorpdev@gmail.com>
2022-11-17 02:02:54 -0300

    feat: Estilização da tela de respostas

--- perguntas/forms.py
+++ perguntas/forms.py
@@ -8,6 +8,11 @@ class PerguntaForm(forms.ModelForm):
     class Meta:
         model = Pergunta
         fields = ['nome', 'pergunta']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['nome'].widget.attrs.update({'class': 'form-control'})
+        self.fields['pergunta'].widget.attrs.update({'class': 'form-control'})
 
 
 class RespostaForm(forms.ModelForm):
@@ -15,3 +20,8 @@ class RespostaForm(forms.ModelForm):
     class Meta:
         model = Resposta
         fields = ['nome', 'resposta']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['nome'].widget.attrs.update({'class': 'form-control'})
+        self.fields['resposta'].widget.attrs.update({'class': 'form-control'})
