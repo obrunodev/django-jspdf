5f5e020a6880daffdc046ae5044cb5545617ac65
obrunodev <brunorpdev@gmail.com>
2022-11-17 00:06:31 -0300

    Reset

--- game/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from game import views
-
-app_name = 'game'
-urlpatterns = [
-    path('', views.index, name='index'),
-]
--- game/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    """Renderiza tela principal do game."""
-    if request.method == 'GET':
-        return render(request, 'game/index.html')
