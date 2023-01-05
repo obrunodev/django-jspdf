93c97a9243d6db75188fea44c19613dea03a7fd4
obrunodev <brunorpdev@gmail.com>
2023-01-05 11:23:37 -0300

    Bootstrap installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
+import { Button, Alert, Container } from 'react-bootstrap';
 import './App.css'
 
 function App() {
-  const [contador, setContador] = useState(0)
+  const variants = ['primary', 'warning', 'danger'];
 
+  const [contador, setContador] = useState(0);
   const handleContador = () => setContador(contador => contador + 1);
 
   return (
-    <div>
+    <Container>
       <p>Contador: {contador}</p>
-      <button onClick={handleContador}>Incrementar</button>
-    </div>
+      <Button variant='primary' onClick={handleContador}>Incrementar</Button>
+      <div className="my-2">
+        {variants.map(variant => (
+          <Alert variant={variant}>Esse é um alerta de {variant}</Alert>
+        ))}
+      </div>
+    </Container>
   )
 }
 
