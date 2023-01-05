f428323d70388af1d57d371f42d2ded3d09987e1
obrunodev <brunorpdev@gmail.com>
2023-01-05 13:49:05 -0300

    feat: Função de comentar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,12 +1,13 @@
-import { Card } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 
 export function Comment({ text }) {
-    return (
-        <Card className='my-2'>
-            <Card.Body>
-                <Card.Title>Usuário X</Card.Title>
-                <p>{ text }</p>
-            </Card.Body>
-        </Card>
-    )
+  return (
+    <Card className='my-2'>
+      <Card.Body>
+        <Card.Title>Usuário X</Card.Title>
+        <p>{ text }</p>
+        <Button variant='dark'>Responder</Button>
+      </Card.Body>
+    </Card>
+  )
 }
